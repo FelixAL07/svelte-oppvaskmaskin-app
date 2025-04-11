adf5abe042163b4674076f810379ad6c46d00e12
FelixAL07 <lerdahl07@gmail.com>
2025-04-11 10:49:21 +0200

    Footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,17 @@
   import { onMount } from "svelte";
   import { getWeekNumber, parseCSV, createDisplayrows } from "$lib/utils";
 
-
   // --- Variables --- \\
   // This is where the data from the CSV will be stored
   let pairs = [];
   let displayRows = [];
-  let loading = true;  // Add loading state
-
+  let loading = true; // Add loading state
 
   // --- Settings --- \\
   // These settings are used to configure the display of the table
   const startWeek = 2;
-  const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQLpWu6sI99utqsllpp-ZjsTvQ1Bww1PkEH-iDfzzXE5-v8qKx4QucxdbMWf0dsh9PFLnoEeSdfriED/pub?output=csv";
+  const sheetUrl =
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQLpWu6sI99utqsllpp-ZjsTvQ1Bww1PkEH-iDfzzXE5-v8qKx4QucxdbMWf0dsh9PFLnoEeSdfriED/pub?output=csv";
   let step = 2;
   let weeksToShow = 14;
 
@@ -49,7 +48,8 @@
       </tr>
     </thead>
     <tbody>
-      {#if loading} <!-- Check if loading -->
+      {#if loading}
+        <!-- Check if loading -->
         {#each Array(weeksToShow) as _, index}
           <tr>
             <td colspan="3"><span class="skeleton"></span></td>
@@ -79,7 +79,7 @@
     max-width: 600px;
     margin: 0 auto;
   }
-  
+
   table {
     width: 100%;
     border-collapse: collapse;
@@ -88,14 +88,14 @@
     border-radius: 8px;
     overflow: hidden;
   }
-  
+
   thead {
     background: linear-gradient(to right, #008a93, #00b4d8);
     color: white;
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
   }
-  
+
   th {
     text-align: center; /* Center align header text */
     font-size: 0.75rem;
@@ -103,45 +103,50 @@
     text-transform: uppercase;
     letter-spacing: 0.05em;
   }
-  
-  td, th {
+
+  td,
+  th {
     padding: 1rem;
     text-align: center; /* Center align cell content */
   }
-  
+
   td {
     white-space: nowrap;
     font-size: 0.875rem;
     color: #4a5568;
   }
-  
+
   tbody tr {
     border-bottom: 2px solid #fade29;
   }
-  
+
   tbody tr:hover {
     background-color: #f1f1f1;
-    transition: background-color 0.2s ease, transform 0.2s ease;
+    transition:
+      background-color 0.2s ease,
+      transform 0.2s ease;
     transform: scale(1.02);
   }
-  
+
   tbody tr:last-child {
     border-bottom: none;
   }
-  
+
   .skeleton {
     display: inline-block;
     height: 0.75rem;
     width: 100%;
     border-radius: 0.25rem;
-    background: linear-gradient(90deg, 
-                hsl(200, 20%, 80%) 25%, 
-                hsl(200, 20%, 95%) 50%, 
-                hsl(200, 20%, 80%) 75%);
+    background: linear-gradient(
+      90deg,
+      hsl(200, 20%, 80%) 25%,
+      hsl(200, 20%, 95%) 50%,
+      hsl(200, 20%, 80%) 75%
+    );
     background-size: 200% 100%;
     animation: shimmer 1.4s ease-in infinite;
   }
-  
+
   @keyframes shimmer {
     0% {
       background-position: -200% 0;
@@ -152,6 +157,9 @@
   }
 
   footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
     background: transparent; /* Transparent background */
     text-align: center;
     padding: 1rem 0;
